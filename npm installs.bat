95b4d59cd1856dd2819f0dac901e7520de5c24f0
Francisco Borges <30115559+V0X4R1N0@users.noreply.github.com>
2019-03-30 20:16:39 +0000

    Add files via upload

---
+++ npm installs.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+echo Starting all downloads :)
+npm install steam-user
+npm install steam-totp
+npm install steamcommunity
+npm install steam-tradeoffer-manager
+echo All done :) Enjoy
